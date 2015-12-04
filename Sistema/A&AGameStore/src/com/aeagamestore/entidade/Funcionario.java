c5c3d0afca2e39c4e0837d6a1c027fadfc69ae4b
xulipaxulepa <aofnas@hotmail.com>
2015-12-04 13:26:57 -0200

    #9 Mapeamento das entidades cargo e funcionário

---
+++ Sistema/A&AGameStore/src/com/aeagamestore/entidade/Funcionario.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.aeagamestore.entidade;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author grupoandroid
+ */
+
+@Entity
+@Table(name = "funcionarios")
+public class Funcionario extends Pessoa implements Serializable {
+    
+    @ManyToOne
+    private Cargo cargo;
+    
+    
+    @Column(nullable = false)
+    private BigDecimal salario;
+
+    public Cargo getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(Cargo cargo) {
+        this.cargo = cargo;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+    
+    
+    
+}

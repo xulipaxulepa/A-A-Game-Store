5942b41b6d247c7a02edc6c0409fb2dff3f6f647
xulipaxulepa <aofnas@hotmail.com>
2015-12-16 21:48:00 -0200

    #10 Criação de Venda, FormaDePagamento e CompraProduto

---
+++ Sistema/A&AGameStore/src/com/aeagamestore/persistencia/CompraProdutoDAO.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.aeagamestore.persistencia;
+
+import com.aeagamestore.entidade.CompraProduto;
+import com.aeagamestore.entidade.repositorios.CompraProdutoRepositorio;
+import java.util.List;
+
+/**
+ *
+ * @author xulipaxulepa
+ */
+public class CompraProdutoDAO extends DAOGenerico<CompraProduto> implements CompraProdutoRepositorio{
+    
+    public CompraProdutoDAO(){
+        super(CompraProduto.class);
+    }
+    
+    @Override
+    public List<CompraProduto> Buscar(CompraProduto filtro) {
+        if(filtro != null){
+            return this.Like("nome", filtro.getFornecedor().getNome())
+                   .Like("nome", filtro.getProduto().getNome())
+                   .IgualA("id", filtro.getId())
+                   .OrderBy("nome", "ASC").Buscar();
+            
+        }
+        return this.Buscar();
+    }
+}

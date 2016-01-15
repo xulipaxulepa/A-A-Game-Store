55a361581e7ba7b260b35af3efd03ee31ce4f48a
xulipaxulepa <aofnas@hotmail.com>
2016-01-15 12:22:56 -0200

    #11 Criação da tela cadastro e funcionário e sua função

--- Sistema_Web/AeAGameStore/src/java/com/aeagamestore/apresentacao/FuncionarioController.java
+++ Sistema_Web/AeAGameStore/src/java/com/aeagamestore/apresentacao/FuncionarioController.java
@@ -5,12 +5,16 @@
  */
 package com.aeagamestore.apresentacao;
 
+import com.aeagamestore.entidade.Cliente;
 import com.aeagamestore.entidade.Funcionario;
+import com.aeagamestore.entidade.Telefone;
 import com.aeagamestore.repositorios.FuncionarioRepositorio;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 
 /**
  *
@@ -25,14 +29,91 @@ public class FuncionarioController {
      */
     @EJB
     FuncionarioRepositorio dao;
+    
+    private Funcionario entidade;
+    
     Funcionario filtro;
+    
+    protected Telefone telefone;
+    
+    protected void MensagemSucesso(String titulo, String msg) {
+        FacesContext context = FacesContext.getCurrentInstance();
+
+        FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_INFO, msg, titulo);
+        context.addMessage(null, m);
+        context.getExternalContext().getFlash().setKeepMessages(true);        
+    }
+    
+    protected void MensagemErro(String titulo, String msg) {
+        FacesContext context = FacesContext.getCurrentInstance();
+
+        FacesMessage m = new FacesMessage(FacesMessage.SEVERITY_ERROR, msg, titulo);
+        context.addMessage(null, m);
+        context.getExternalContext().getFlash().setKeepMessages(true);        
+    }
+
+    public Telefone getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(Telefone telefone) {
+        this.telefone = telefone;
+    }
     
     public FuncionarioController() {
         filtro = new Funcionario();
+        telefone = new Telefone();
+        entidade = new Funcionario();
     }
     
+    public void limparCampos(){
+        this.entidade = new Funcionario();
+        this.telefone = new Telefone();
+    }
+    
+    public void salvar(){
+        //entidade.addTelefone(telefone);
+        if(dao.Salvar(entidade)){
+            this.limparCampos();
+            MensagemSucesso("Sucesso!", "Registro salvo com sucesso!");
+        }else
+            MensagemErro("Falha!", "Erro ao salvar o registro. Contacte o administrador do sistema!");
+    }
+    
+    public String apagar(){
+        if(dao.Apagar(entidade))
+            return "ClienteListagem.xhtml";
+        else {
+            MensagemErro("Falha!", "Erro ao apagar o registro. Contacte o administrador do sistema!");
+            return "";
+        }
+    }
+    
+    public void filtrar(){
+        
+    }
+    
+    public void limpar(){
+        filtro = new Funcionario();
+    }
     
     public List<Funcionario> getListagem(){
         return dao.Buscar(null);
+    }
+    
+    public Funcionario getFiltro() {
+        return filtro;
+    }
+
+    public void setFiltro(Funcionario filtro) {
+        this.filtro = filtro;
+    }
+
+    public Funcionario getEntidade() {
+        return entidade;
+    }
+
+    public void setEntidade(Funcionario entidade) {
+        this.entidade = entidade;
     }
 }

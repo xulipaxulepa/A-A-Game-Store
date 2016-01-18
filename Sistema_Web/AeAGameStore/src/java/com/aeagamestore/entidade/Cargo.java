7ba1829a6abd75bf22275fac904c13d37e047de9
ArleyOliveira <arley.msn@hotmail.com>
2016-01-18 20:59:41 -0200

    #12 Alteração tela cadastrar e editar cliente

--- Sistema_Web/AeAGameStore/src/java/com/aeagamestore/entidade/Cargo.java
+++ Sistema_Web/AeAGameStore/src/java/com/aeagamestore/entidade/Cargo.java
@@ -6,6 +6,7 @@
 package com.aeagamestore.entidade;
 
 import java.io.Serializable;
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -62,6 +63,31 @@ public class Cargo implements Serializable {
 
     public void setObservacao(String observacao) {
         this.observacao = observacao;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 47 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cargo other = (Cargo) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
     }
 
 }
